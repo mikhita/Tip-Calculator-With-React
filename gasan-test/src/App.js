0d363bb9c88ac790a8fecac6fe2ae74c5d8c35a3
mikhita <mikheilispiriani@yahoo.com>
2022-09-16 21:03:17 +0400

    I add components

--- gasan-test/src/App.js
+++ gasan-test/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import NAvbar from './navbar';
+import Home from './home';
 
 function App() {
   return (
     <div className="App">
+      <NAvbar/>
       <div className="content">
-        <h1>hello app</h1>
+      <Home/>
       </div>
     </div>
   );
---
+++ gasan-test/src/navbar.js
@@ -0,0 +1,13 @@
+const NAvbar = () => {
+    return ( 
+        <nav className="navbarNav">
+            <h1 className="navBarH1">The Mikhitas blog</h1>
+            <div className="navbarContent">
+                <a href="/" className="homaAnchor">Home</a>
+                <a href="/" className="homaAnchor">New Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default NAvbar;
