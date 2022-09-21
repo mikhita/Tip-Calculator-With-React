e7d51179e1bc40afcc0513e38c36eeae4364df97
mikhita <mikheilispiriani@yahoo.com>
2022-09-22 03:06:56 +0400

    I add styles to the blogs

--- gasan-test/src/BlogDetails.js
+++ gasan-test/src/BlogDetails.js
@@ -1,9 +1,21 @@
 import {useParams} from "react-router-dom"
+import useFetch from "./useFetch";
 const BlogDetails = () => {
     const {id} = useParams()
+    const {data:blog,isLoading,error} = useFetch("http://localhost:3000/blogs/"+id)
+
     return ( 
         <div className="blog-details">
-            <h2>blogs list - {id}</h2>
+            { isLoading && <div>Loading...</div>}
+            {error && <div>{  error  }</div>}
+            {blog && <div>
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            </div>}
+
         </div>
      );
 }
